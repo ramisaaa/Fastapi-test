bc5c4c626570f99b84e88672df16a281a1039f65
ramisaheidari <ramisaheidari@gmail.com>
2023-05-27 20:09:50 +0200

    remove_dataset function added & some improvements on the other parts

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 from starlette.responses import JSONResponse
+from pydantic import BaseModel
 import pandas as pd
 import uuid
 import os
@@ -11,6 +12,12 @@ datasets = {}
 UPLOAD_FOLDER = "uploads"
 
 
+class Dataset(BaseModel):
+    id: str
+    filename: str
+    size: int
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -18,12 +25,18 @@ async def root():
 
 @app.get("/datasets/")
 def list_datasets():
-    file_lists = []
-    for file_name in os.listdir(UPLOAD_FOLDER):
-        file_path = os.path.join(UPLOAD_FOLDER, file_name)
+    datasets = []
+
+    for file in os.listdir(UPLOAD_FOLDER):
+        file_path = os.path.join(UPLOAD_FOLDER, file)
         if os.path.isfile(file_path):
-            file_lists.append(file_name)
-    return {"datasets": file_lists}
+            dataset = Dataset(
+                id=str(file.split('.')[0]),
+                filename=file,
+                size=os.path.getsize(file_path)
+            )
+            datasets.append(dataset)
+    return datasets
 
 
 @app.post("/datasets/")
@@ -40,8 +53,11 @@ async def create_dataset(file: UploadFile = File(...)):
         # Read CSV file and store it as a pandas dataframe
         df = pd.read_csv(file_path)
         # Store the dataset in memory
-        datasets[dataset_id] = df
-        return {"dataset_id": dataset_id}
+        return Dataset(
+            id=str(file_name.split('.')[0]),
+            filename=file_name,
+            size=os.path.getsize(file_path)
+        )
     except Exception as e:
         return JSONResponse(status_code=400, content={"error": str(e)})
 
@@ -55,4 +71,18 @@ async def get_dataset_info(id: str):
         return JSONResponse(status_code=404, content={"error": "dataset file not found"})
 
     file_size = os.path.getsize(file_path)
-    return {"file_name": file_name, "file_size": file_size}
+    return Dataset(
+        id=str(file_name.split('.')[0]),
+        filename=file_name,
+        size=os.path.getsize(file_path)
+    )
+
+
+@app.delete("/datasets/{id}")
+async def remove_dataset(id: str):
+    file_path = os.path.join(UPLOAD_FOLDER, f"{id}.csv")
+    if os.path.exists(file_path) and os.path.isfile(file_path):
+        os.remove(file_path)
+        return JSONResponse(status_code=200, content={"success": "dataset removed"})
+    else:
+        return JSONResponse(status_code=404, content={"error": "dataset file not found"})
