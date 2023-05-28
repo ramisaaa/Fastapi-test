95bf50a140459a1719fe56a5d8c7db8d5b861eab
ramisaheidari <ramisaheidari@gmail.com>
2023-05-28 21:40:44 +0200

    test -------------- test for create_dataset added

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 from .main import app
-import pytest
+import os
 
 
 client = TestClient(app)
@@ -16,6 +16,26 @@ def test_list_datasets():
         assert "filename" in dataset
         assert "size" in dataset
         assert dataset["filename"].endswith(".csv")
+
+
+def test_create_dataset():
+    # Create a sample CSV file for testing
+    temp_csv_file = "test_dataset.csv"
+    with open(temp_csv_file, "w") as f:
+        f.write("col1,col2\nvalue1,value2\n")
+
+    with open(temp_csv_file, "rb") as csv_file:
+        response = client.post("/datasets/", files={"file": csv_file})
+
+    assert response.status_code == 200
+    dataset = response.json()
+    assert "id" in dataset
+    assert "filename" in dataset
+    assert "size" in dataset
+    assert dataset["filename"].endswith(".csv")
+
+    # remove test CSV file
+    os.remove(temp_csv_file)
 
 
 
