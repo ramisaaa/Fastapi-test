2b77e03204cda96e9c5e079b86cd96a61cc589c7
ramisaheidari <ramisaheidari@gmail.com>
2023-05-28 21:24:27 +0200

    test -------------- test for list_datasets added

--- app/test_main.py
+++ app/test_main.py
@@ -1,12 +1,21 @@
-
 from fastapi.testclient import TestClient
-
 from .main import app
+import pytest
+
 
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
+def test_list_datasets():
+    response = client.get("/datasets/")
     assert response.status_code == 200
-    assert response == "Ramisa Heidari :)"
+    datasets = response.json()
+    assert isinstance(datasets, list)
+    for dataset in datasets:
+        assert "id" in dataset
+        assert "filename" in dataset
+        assert "size" in dataset
+        assert dataset["filename"].endswith(".csv")
+
+
+
